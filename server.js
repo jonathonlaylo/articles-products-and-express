5e6f3a2a10112c1436fe60ca2867a6c7b66701f4
jonathonlaylo <jonathon.laylo@gmail.com>
2017-01-11 21:59:57 -1000

    server is listening to me, you, and port 4000

---
+++ server.js
@@ -0,0 +1,8 @@
+/*jshint esversion: 6 */
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () =>{
+  console.log('Server is listening', PORT);
+});
