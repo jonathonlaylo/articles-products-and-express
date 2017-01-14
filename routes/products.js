a8e816b073a04a0ac05dc9249eb0b21ac82627c6
jonathonlaylo <jonathon.laylo@gmail.com>
2017-01-13 22:16:59 -1000

    routes/products.js postman works. created array for productList

--- routes/products.js
+++ routes/products.js
@@ -1,17 +1,34 @@
 /*jshint esversion: 6 */
 const express = require('express');
 const router = express.Router();
-// const dbProducts = require('../db/products');
+const productList = [];
+
+// testing to see if Postman is working
+// router.get('/', (req, res)=>{
+//   res.send('are you working');
+// });
 
 router.get('/', (req, res)=>{
-  res.send('are you working');
+  res.send({'productList': productList});
 });
 
-// products.route('/')
-// .post((res, req) =>{
-//   dbProducts.add(req.body);
-//   res.json({'sucess': true});
-// });
+router.post('/', (req, res)=>{
+  // console.log(req.body);
+  let newProducts = req.body;
+  let data = {
+    // id: newProducts.id,
+    name: newProducts.name,
+    price: newProducts.price,
+    inventory: newProducts.inventory
+  };
+  productList.push(data);
+  console.log(productList);
+  res.end();
+});
+
+
+
+
 
 
 module.exports = router;
--- server.js
+++ server.js
@@ -3,7 +3,23 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const products = require('./routes/products');
+const handlebars = require('express-handlebars');
+
+const hbs = handlebars.create({
+  extname: '.hbs',
+  defaultlayout: 'app'
+});
+
+app.engine('hbs', hbs.engine);
+
+app.set('view engine', 'hbs');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/products', products);
+
+app.get('/', (req, res) =>{
+  console.log('test test testies');
+});
 
 module.exports = app;
