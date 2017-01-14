dceeb32aa482fa325e7ad0b3e8f291a688033350
jonathonlaylo <jonathon.laylo@gmail.com>
2017-01-14 12:10:58 -1000

    post is working. added unique IDs

--- routes/products.js
+++ routes/products.js
@@ -1,7 +1,9 @@
 /*jshint esversion: 6 */
 const express = require('express');
 const router = express.Router();
+const querystring = require('querystring');
 const productList = [];
+const counter = {count : 0};
 
 // testing to see if Postman is working
 // router.get('/', (req, res)=>{
@@ -14,20 +16,46 @@ router.get('/', (req, res)=>{
 
 router.post('/', (req, res)=>{
   let newProducts = req.body;
+  // let idCounter = counter.count++;
   let data = {
-    // id: newProducts.id,
+    id: counter.count++,
     name: newProducts.name,
     price: newProducts.price,
     inventory: newProducts.inventory
   };
   productList.push(data);
-  console.log(productList);
-  res.end();
+  console.log('productList', productList);
+  console.log(data.id);
+  // console.log(req.body.id);
+  res.json(productList);
+});
+
+router.put('/:id', (req, res)=>{
+  let newProducts = req.body;
+  for (var i = 0; i < productList.length; i++) {
+    if(data.id === productList[i].id){
+
+    }
+  }
+});
+
+router.delete('/:id', (req, res)=>{
+  let newProducts = req.body;
+  let removeID = req.body.id;
+
+  // if statement to remove the id
+  if (true) {
+
+  }
 });
 
 
-
-
-
-
 module.exports = router;
+    //something something hasOwnProperty dafuq do i know
+    // if(updateID.hasOwnProperty(newProducts.name)){
+
+    // } else if(updateID.hasOwnProperty(newProducts.price)){
+
+    // } else if(updateID.hasOwnProperty(newProducts.inventory)){
+
+    // }
--- server.js
+++ server.js
@@ -2,13 +2,15 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const products = require('./routes/products');
 const handlebars = require('express-handlebars');
+const products = require('./routes/products');
+const articles = require('./routes/articles');
 
 const hbs = handlebars.create({
   extname: '.hbs',
   defaultlayout: 'app'
 });
+
 
 app.engine('hbs', hbs.engine);
 
@@ -18,8 +20,11 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/products', products);
 
-app.get('/', (req, res) =>{
-  console.log('test test testies');
-});
+
+// app.get('/', (req, res) =>{
+//   res.render('products', products);
+//   // counter.count++;
+//   // res.send(counter);
+// });
 
 module.exports = app;
