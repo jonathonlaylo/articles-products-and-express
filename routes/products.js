6d0806bc31bb4b98eed8d6896399ed32866894c3
jonathonlaylo <jonathon.laylo@gmail.com>
2017-01-14 16:21:18 -1000

    products now puts in name, price, or inventory

--- routes/products.js
+++ routes/products.js
@@ -20,23 +20,51 @@ router.post('/', (req, res)=>{
   let data = {
     id: counter.count++,
     name: newProducts.name,
-    price: newProducts.price,
-    inventory: newProducts.inventory
+    price: parseInt(newProducts.price),
+    inventory: parseInt(newProducts.inventory)
   };
   productList.push(data);
-  console.log('productList', productList);
-  console.log(data.id);
+  console.log(productList);
+  // console.log('data.id', data.id);
   // console.log(req.body.id);
   res.json(productList);
 });
 
 router.put('/:id', (req, res)=>{
+  // res.send("are you working");
   let newProducts = req.body;
-  for (var i = 0; i < productList.length; i++) {
-    if(data.id === productList[i].id){
+  // console.log(newProducts);
+  let newID = req.body.id;
+  // console.log(newID);
+  let addressID = req.params.id;
+  // console.log(addressID);
+  // console.log('req.body', newProducts.id, newProducts.name);
+  // console.log(productList[0].name);
 
-    }
+  if(newProducts.name){
+    productList[addressID].name = newProducts.name;
+    res.json(productList[newID].name);
+    console.log(productList);
   }
+  if(newProducts.price){
+    productList[addressID].price = newProducts.price;
+    res.json(productList[newID].name);
+    console.log(productList);
+  }
+  if(newProducts.inventory){
+    productList[addressID].inventory = newProducts.inventory;
+    res.json(productList[newID].inventory);
+    console.log(productList);
+  }
+    // if(newProducts.id === productList[i].id){
+    //   let newName = newProducts.name;
+    //   console.log(newName);
+    //   productList[i].name = newName;
+    //   res.render(productList[i].name);
+    //   console.log('put productList', productList[i].name);
+    // }
+    // console.log("test");
+
 });
 
 router.delete('/:id', (req, res)=>{
