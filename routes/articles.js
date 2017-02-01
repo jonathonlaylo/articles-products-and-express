8d84116ced54b3c1d5e1c7a377aedefdc664991b
jonathonlaylo <jonathon.laylo@gmail.com>
2017-01-31 21:30:31 -1000

    added article routes route.

--- routes/articles.js
+++ routes/articles.js
@@ -8,23 +8,50 @@ const router = express.Router();
 
 const articlesList = articles.articlesList;
 
-let idCounter = 0;
+router.route('/')
+  .get((req, res)=>{
 
-router.post('/', (req, res)=>{
-  let newArticles = req.body;
-  let urlEncode = encodeURIComponent(String(req.body.title));
+  })
+  .post((req, res)=>{
 
-  if(newArticles.title && newArticles.body && newArticles.author){
-      newArticles.urlTitle = urlEncode;
+  });
 
-      articlesList.push(newArticles);
-      console.log(articlesList);
-      // res.send(newArticles);
-      // let postArticles = res.redirect('/articles');
-  } else {
-    res.send('Whoops');
-  }
-});
+router.route('/new')
+  .get((req, res)=>{
+
+  });
+
+router.route('/:title')
+  .get((req, res)=>{
+
+  })
+  .put((req, res)=>{
+
+  })
+  .delete((req, res)=>{
+
+  });
+
+router.route('/:title/edit')
+  .get((req, res)=>{
+
+  });
+
+// router.post('/', (req, res)=>{
+//   let newArticles = req.body;
+//   let urlEncode = encodeURIComponent(String(req.body.title));
+
+//   if(newArticles.title && newArticles.body && newArticles.author){
+//       newArticles.urlTitle = urlEncode;
+
+//       articlesList.push(newArticles);
+//       console.log(articlesList);
+//       // res.send(newArticles);
+//       // let postArticles = res.redirect('/articles');
+//   } else {
+//     res.send('Whoops');
+//   }
+// });
 
 
 
